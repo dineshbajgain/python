9e74e80ee78e4dd99dc6ecefcf812787f19e9679
dinesh.bajgain <dinesh.bajgain@aayulogic.com>
2021-10-08 10:03:22 +0545

    strings multiple line

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-birth_year = input('Birth year : ')
-age = 2021-int(birth_year)
-print(age)
-
-# assignment
-
-pound = input('Enter your weight in pounds:')
-kilogram = float(pound) / 2.20462
-print(kilogram)
+course =''''
+Hi john here is an email to you 
+how are you 
+djh
+'''
+print(course)
