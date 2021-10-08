c834f5dd5779c278cd2c67a46f273ee1effcfad8
dinesh.bajgain <dinesh.bajgain@aayulogic.com>
2021-10-08 10:00:42 +0545

    type conversion

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-name = input('what is your name? ')
-color = input('what is your favourite color?')
-print(name+ ' likes ' +color)
+birth_year = input('Birth year : ')
+age = 2021-int(birth_year)
+print(age)
+
+# assignment
+
+pound = input('Enter your weight in pounds:')
+kilogram = float(pound) / 2.20462
+print(kilogram)
